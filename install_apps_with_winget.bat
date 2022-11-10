a2d774f2b05e92ca31cc90723b4f9f007a1e1422
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-10 02:54:59 -0500

    Update install_apps_with_winget.bat

--- install_apps_with_winget.bat
+++ install_apps_with_winget.bat
@@ -1,8 +1,19 @@
-#
 
-# https://learn.microsoft.com/ja-jp/windows/package-manager/winget/
+::https://learn.microsoft.com/ja-jp/windows/package-manager/winget/
 
-winget install inkscape
+
 winget install -e --id Git.Git
+
+winget install -e --id ProtonTechnologies.ProtonVPN
+winget install -e --id TorProject.TorBrowser
+
+winget install -e --id GIMP.GIMP
+winget install inkscape
+
+winget install -e --id AOMEI.Backupper
+winget install -e --id KeePassXCTeam.KeePassXC
+
+winget install -e --id Oracle.VirtualBox
+winget install -e --id CPUID.CPU-Z
 
 
