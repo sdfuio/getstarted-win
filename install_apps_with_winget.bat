238fb7f92314f5a401f0a95c917387633444ae22
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-10 03:06:46 -0500

    Update install_apps_with_winget.bat

--- install_apps_with_winget.bat
+++ install_apps_with_winget.bat
@@ -13,13 +13,24 @@ winget install -e --id Microsoft.dotNetFramework
 
 winget install -e --id ProtonTechnologies.ProtonVPN
 winget install -e --id TorProject.TorBrowser
+winget install -e --id Mozilla.Firefox
+winget install -e --id BraveSoftware.BraveBrowser
+
 
 winget install -e --id GIMP.GIMP
 winget install inkscape
+winget install -e --id VideoLAN.VLC
+winget install -e --id Audacity.Audacity
+winget install -e --id LIGHTNINGUK.ImgBurn
+winget install -e --id CodecGuide.K-LiteCodecPack.Full
+
+winget install -e --id Kiwix.WikiMed
+winget install -e --id calibre.calibre
 
 winget install -e --id AOMEI.Backupper
 winget install -e --id KeePassXCTeam.KeePassXC
 winget install -e --id GnuPG.Gpg4win
+winget install -e --id WiresharkFoundation.Wireshark
 
 
 winget install -e --id Oracle.VirtualBox
