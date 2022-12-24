2a123a6c20c57394f4e7bdaf959413de91f1e590
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-12-24 19:11:25 +0900

    Update install_apps_with_winget.bat

--- install_apps_with_winget.bat
+++ install_apps_with_winget.bat
@@ -35,6 +35,78 @@ winget install -e --id WiresharkFoundation.Wireshark
 winget install -e --id IDRIX.VeraCrypt
 winget install -e --id AdGuard.AdGuard
 
+Skip to content
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+@sdfuio
+sdfuio /
+getstarted-win
+Public
+
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+
+    Settings
+
+getstarted-win/install_apps_with_winget.bat
+@sdfuio
+sdfuio Update install_apps_with_winget.bat
+Latest commit 7b85cd6 Nov 10, 2022
+History
+1 contributor
+42 lines (33 sloc) 1.39 KB
+
+:: https://learn.microsoft.com/ja-jp/windows/package-manager/winget/
+:: https://winget.run/
+
+winget install -e --id Git.Git
+winget install -e --id Codeblocks.Codeblocks
+winget install -e --id Spyder.Spyder
+winget install -e --id Azul.Zulu.18.JRE
+winget install -e --id Azul.Zulu.18.JDK
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Microsoft.dotNetFramework
+winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
+
+winget install -e --id ProtonTechnologies.ProtonVPN
+winget install -e --id TorProject.TorBrowser
+winget install -e --id Mozilla.Firefox
+winget install -e --id BraveSoftware.BraveBrowser
+
+
+winget install -e --id GIMP.GIMP
+winget install inkscape
+winget install -e --id VideoLAN.VLC
+winget install -e --id Audacity.Audacity
+winget install -e --id LIGHTNINGUK.ImgBurn
+winget install -e --id CodecGuide.K-LiteCodecPack.Full
+
+winget install -e --id Kiwix.WikiMed
+winget install -e --id calibre.calibre
+winget install -e --id TheDocumentFoundation.LibreOffice
+
+winget install -e --id AOMEI.Backupper
+winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id GnuPG.Gpg4win
+winget install -e --id WiresharkFoundation.Wireshark
+winget install -e --id IDRIX.VeraCrypt
+winget install -e --id AdGuard.AdGuard
+
+winget install -e --id Apple.iTunes
+winget install -e --id Oracle.VirtualBox
+winget install -e --id CPUID.CPU-Z
+winget install -e --id Dell.CommandUpdate.Universal
+winget install -e --id GnuPG.Gpg4win
+
 winget install -e --id Apple.iTunes
 winget install -e --id Oracle.VirtualBox
 winget install -e --id CPUID.CPU-Z
